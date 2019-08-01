09907e3d6eecee30fa1d2699f3c413217585aca4
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2019-07-31 20:18:23 -0600

    Update vistas
    Vistas para perfiles de Usuario,
    las Hice por separado dependiendo del tipo de Usuario en el sistema

--- Documentacion/Vistas/Vistas.sql
+++ Documentacion/Vistas/Vistas.sql
@@ -1,4 +1,30 @@
 /*VISTAS DE LOS TOUR POPULARES*/
-create view view_populares as 
+CREATE view view_populares as 
 SELECT t.idTours, t.nombre, t.descripcion, t.calificacion 
 FROM `tours` t inner join populares p oN p.idpopulares=t.idtours
+
+/*VISTA DE LOS PERFILES DE USUARIO*/
+
+/*ADMINISTRADOR*/
+CREATE VIEW VIEW_Perfil_Usuario_Admin AS
+SELECT p.idPersona, u.idUsuario, p.nombreCompleto, p.Apellidos, p.numeroIdentidad, p.telefono, p.genero, p.direccion,
+	   u.nombreUsuario, u.email
+FROM persona p 
+INNER JOIN usuario u ON p.idPersona = u.idUsuario
+INNER JOIN administrador a ON a.idUsuario = u.idUsuario;
+
+/*TURISTA*/
+CREATE VIEW VIEW_Perfil_Usuario_Turista AS
+SELECT p.idPersona, u.idUsuario, p.nombreCompleto, p.Apellidos, p.numeroIdentidad, p.telefono, p.genero, p.direccion,
+	   u.nombreUsuario, u.email
+FROM persona p 
+INNER JOIN usuario u ON p.idPersona = u.idUsuario
+INNER JOIN turista t ON t.idUsuario = u.idUsuario;
+
+/*GUIA*/
+CREATE VIEW VIEW_Perfil_Usuario_Admin AS
+SELECT p.idPersona, u.idUsuario, p.nombreCompleto, p.Apellidos, p.numeroIdentidad, p.telefono, p.genero, p.direccion,
+	   u.nombreUsuario, u.email
+FROM persona p 
+INNER JOIN usuario u ON p.idPersona = u.idUsuario
+INNER JOIN guia g ON g.idUsuario = u.idUsuario;
