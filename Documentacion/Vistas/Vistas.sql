752650806015ed11a950383c887eb2d62c59c7d0
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2019-08-07 08:22:43 -0600

    update vistas

--- Documentacion/Vistas/Vistas.sql
+++ Documentacion/Vistas/Vistas.sql
@@ -1,7 +1,7 @@
 /*VISTAS DE LOS TOUR POPULARES*/
 CREATE view view_populares as 
 SELECT t.idTours, t.nombre, t.descripcion, t.calificacion 
-FROM `tours` t inner join populares p oN p.idpopulares=t.idtours
+FROM `tours` t inner join populares p oN p.idpopulares=t.idtours;
 
 /*VISTA DE LOS PERFILES DE USUARIO*/
 
@@ -29,44 +29,22 @@ INNER JOIN usuario u ON p.idPersona = u.idUsuario
 INNER JOIN turista t ON t.idUsuario = u.idUsuario;
 
 /*GUIA*/
-CREATE VIEW VIEW_Perfil_Usuario_Admin AS
+CREATE VIEW VIEW_Perfil_Usuario_Guia AS
 SELECT p.idPersona, u.idUsuario, p.nombreCompleto, p.Apellidos, p.numeroIdentidad, p.telefono, p.genero, p.direccion,
 	   u.nombreUsuario, u.email
 FROM persona p 
 INNER JOIN usuario u ON p.idPersona = u.idUsuario
 INNER JOIN guia g ON g.idUsuario = u.idUsuario;
 
-
-/*vista de guia y el tour   */ 
-create or replace view view_guiaTours as select T.nombre as nombreTour, Gu.nombreCompleto as Guia, idTours as idTours from tours as T
-	inner join view_guia as Gu
-	on T.idGuia= Gu.idguia;
-    
-#vista de Turistas con guias
-create or replace view view_turistaTourGuia  as select 
-	Tu.nombreCompleto as nombreTurista, Tour.nombreTour,Tour.Guia from toursturista as tourt
-	inner join view_turistaTours  as Tu
-
-	on tourt.idTurista=Tu.idTurista
-
-	inner join  view_guiaTours as Tour
-	on tourt.idTours=Tour.idTours
-	where tourt.idTurista  is not null;
-    
-
 /*========================================================================*/
 /*VISTAS DEL DASHBOARD*/
 CREATE VIEW tours_dashboard  AS 
 SELECT t.idtours id, t.nombre Nombre_Tour, t.precio Precio_Tours FROM tours t 
-INNER JOIN hotel h ON h.idtours=t.idtours
-SELECT t.idtours id, t.nombre Nombre_Tour, t.precio Precio_Tours FROM 
-tours t INNER JOIN estados e ON e.idEstados=t.idestados 
-
+INNER JOIN hotel h ON h.idtours=t.idtours;
 
 /*VISTAS DEL DASHBOARD-DETALLES*/
-drop view detalles_tours;
 CREATE VIEW detalles_tours AS
 SELECT t.idtours id, t.descripcion, t.nombre Nombre_Tour, e.nombre Nombre_Estado, t.calificacion, t.precio Precio_Tours, t.cupos, u.nombreUsuario Usuario, t.fechaInicio, t.fechaFin, g.idGuia, e.idEstados FROM tours t
 INNER JOIN estados e ON e.idEstados=t.idestados
 INNER JOIN guia g ON g.idguia=t.idguia
-INNER JOIN usuario u ON u.idusuario=g.idusuario
+INNER JOIN usuario u ON u.idusuario=g.idusuario;
