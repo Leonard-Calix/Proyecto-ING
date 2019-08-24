1c1c000ceb77f82decc3222c8ef387a958d39303
Leonardo-Calix <bcalixvelasquez@gmail.com>
2019-08-24 01:03:31 -0600

    nav

--- Vista/Layouts/navbar.php
+++ Vista/Layouts/navbar.php
@@ -1,4 +1,4 @@
-<?php // session_start(); ?>
+<?php session_start(); ?>
 
 <!-- NAVBAR ================================================= -->
 <nav class="navbar navbar-expand-xl navbar-dark  navbar-togglable fixed-top">
