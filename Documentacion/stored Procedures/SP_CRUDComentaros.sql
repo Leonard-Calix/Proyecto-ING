9255883465838ed2dd23fcedccadd4316e82b406
CatherineVal <38821147+CatherineVal@users.noreply.github.com>
2019-08-26 19:01:47 -0600

    Update SP_CRUDComentaros.sql

--- Documentacion/stored Procedures/SP_CRUDComentaros.sql
+++ Documentacion/stored Procedures/SP_CRUDComentaros.sql
@@ -20,3 +20,39 @@ END IF;
 END$$
 
 DELIMITER ;
+
+
+
+/*PROCEDIMMIENTO AGREGAR COMENTARIO*/
+DELIMITER $$
+
+CREATE PROCEDURE ADD_COMENTARIOS(IN pcomentario VARCHAR(600), IN pidUsuario INT, IN pidTours INT, OUT pMensaje INT)
+
+BEGIN
+	DECLARE pError VARCHAR(600);
+	SET pError = '';
+	
+	IF pcomentario = '' THEN
+		SET pError = CONCAT(pError, ' ','Comentario vacio');
+	END IF;
+	IF pidUsuario = null THEN
+		SET pError = CONCAT(pError, ' ','idUsuario vacio');
+	END IF;
+	IF pidTours = null THEN
+		SET pError = CONCAT(pError, ' ','idTours vacio');
+	END IF;
+
+		IF pError = '' THEN
+
+		INSERT INTO comentarios(idComentarios,comentario, fechaComentario, horaComentario, idUsuario, idTours )
+					VALUES(null,pcomentario, CURDATE(), curTime(),  pidUsuario, pidTours);
+
+			SET pMensaje = 1;
+		ELSE
+
+		SET pMensaje = 0;
+
+	END IF;
+END$$
+
+DELIMITER ;
